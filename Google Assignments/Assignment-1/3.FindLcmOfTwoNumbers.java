8aba57bb10b5c729a3efe53549cc4a71eafe2fc2
Balaji1252 <balajiran.n2020cse@sece.ac.in>
2022-09-16 09:49:31 +0530

    commit

--- Google Assignments/Assignment-1/3.FindLcmOfTwoNumbers.java
+++ Google Assignments/Assignment-1/3.FindLcmOfTwoNumbers.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class HelloWorld {
-    public static void main(String[] args) {
+    public  static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
        int gcd=0,lcm=0;
        int x=sc.nextInt();
