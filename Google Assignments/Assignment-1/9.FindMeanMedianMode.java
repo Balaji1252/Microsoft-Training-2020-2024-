1ca6cf13d2e5baae4cede5508f8496b9b827d550
Balaji1252 <balajiran.n2020cse@sece.ac.in>
2022-09-18 20:35:50 +0530

    commit

--- Google Assignments/Assignment-1/9.FindMeanMedianMode.java
+++ Google Assignments/Assignment-1/9.FindMeanMedianMode.java
@@ -1,1 +1,35 @@
 import java.util.*;
+class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0,ctr=0,max=0,mode=0,median=0;
+		int[] arr=new int[n];
+		int l=0,r=n-1;
+		int mid=l+(r-l)/2;
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		    sum+=arr[i];
+		 }
+		 Arrays.sort(arr);
+		 for(int i=0;i<n;i++){
+		     median=arr[mid];
+		 }
+		 for(int i=0;i<n;i++){
+		     for(int j=i+1;j<n;j++){
+		         if(arr[i]==arr[j]){
+		             ctr++;
+		         }
+		     }
+		     if(max<ctr){
+		         max=ctr;
+		         mode=arr[i];
+		     }
+		 }
+		 
+		System.out.println("Mean is " + sum/n);
+		System.out.println("Median is " + median);
+		System.out.println("Mode is " + mode);
+	}
+}
