ea7a194b56240f2c1130c8e2383edd6b20cb47ce
Balaji1252 <balajiran.n2020cse@sece.ac.in>
2022-10-09 17:44:01 +0530

    commit

--- Microsoft/Homework Assignments/Week-3/6.iterativePostOrderTraversal.java
+++ Microsoft/Homework Assignments/Week-3/6.iterativePostOrderTraversal.java
@@ -1,1 +1,35 @@
+import java.util.ArrayList;
+import java.util.Stack;
 
+// User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+class Tree {
+    ArrayList<Integer>postOrder(Node node){
+    ArrayList<Integer>list=new ArrayList<>();
+    Stack<Node>stack=new Stack<>();
+    stack.push(node);
+    while(!stack.isEmpty()){
+        Node cur=stack.pop();
+        list.add(0,cur.data);
+        if(cur.left!=null){
+            stack.push(cur.left);
+        }
+        if(cur.right!=null){
+            stack.push(cur.right);
+        }
+    }
+    return  list;
+   }
+}
