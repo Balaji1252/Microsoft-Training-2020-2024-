37578e7ab530df87847273d14e680108b94cc042
Balaji1252 <balajiran.n2020cse@sece.ac.in>
2022-10-12 14:43:39 +0530

    commit

--- Microsoft/In-Class Assignments/Week-4/2.binaryTreeInorder.java
+++ Microsoft/In-Class Assignments/Week-4/2.binaryTreeInorder.java
@@ -4,7 +4,8 @@ import java.util.List;
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer>list = new ArrayList<>();
-        if(root==null){
+        if(root==null)
+        {
             return list ;
         }
         list.addAll(inorderTraversal(root.left));
